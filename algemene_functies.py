2973f5442c2026076fc80135fdc6b7bb6677fb82
Myrthe van Hussel <myrthevanhussel@gmail.com>
2026-03-23 12:07:40 +0100

    project gemaakt

---
+++ algemene_functies.py
@@ -0,0 +1,13 @@
+def mijn_functie_1(a):
+    return a**2
+x = 4
+totaal = mijn_functie_1(x)
+#print(totaal)
+
+def mijn_functie_2(a,b):
+    return [a + b, a - b, a * b, a / b]
+
+m = 12
+n = 2
+totaal_2 = mijn_functie_2(m,n)
+print(totaal_2)
